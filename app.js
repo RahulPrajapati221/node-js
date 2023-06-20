6d9293bd2d6b7db17d722482261b25a2575ee817
WEB-H-060 <7prajapatirahul@gmail.com>
2023-06-20 17:51:22 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require ("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const db_uri = "mongodb://localhost:27017"
+
+const db = mongoose.connect(db_uri,()=>{
+    console.log("database connected....")
+})
+
+app.listen(5000,()=>{
+    console.log("server running....")
+})
